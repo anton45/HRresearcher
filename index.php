5bdf84f72c423e8f3e72b47f41331b2f3d36e5e8
Anton Goleusov <antongoleusov@gmail.com>
2022-02-05 16:38:13 +0300

    last

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Answer;
+use App\Question;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
@@ -17,9 +18,15 @@ require_once __DIR__ . "/vendor/autoload.php";
 //$answer = new Answer(0, 'yes', 2);
 //var_dump($answer);
 
-$answerJsonstring = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'answers.json');
-$answerJson = json_decode($answerJsonstring);
-foreach ($answerJson as $numberAswer => $aswerObject) {
-    $answer = new Answer($aswerObject->id, $aswerObject->answerText, $aswerObject->point);
-    var_dump($answer);
+//$answerJsonstring = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'answers.json');
+//$answerJson = json_decode($answerJsonstring);
+//foreach ($answerJson as $numberAswer => $aswerObject) {
+//    $answer = new Answer($aswerObject->id, $aswerObject->answerText, $aswerObject->point);
+//    var_dump($answer);
+//}
+$questionsJsonstring = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'questions.json');
+$questionsJson = json_decode($questionsJsonstring);
+foreach ($questionsJson as $elementJson => $questionObject) {
+    $questions = new Question($questionObject->id, $questionObject->questionText, $questionObject->answers);
+    var_dump($questionObject);
 }
