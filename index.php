681b9944946625e436e4f4c1aaecb97ab3d4c791
Anton Goleusov <antongoleusov@gmail.com>
2022-02-05 20:27:25 +0300

    /

--- index.php
+++ index.php
@@ -26,7 +26,13 @@ require_once __DIR__ . "/vendor/autoload.php";
 //}
 $questionsJsonstring = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'questions.json');
 $questionsJson = json_decode($questionsJsonstring);
-foreach ($questionsJson as $elementJson => $questionObject) {
-    var_dump($questionObject->id);
-    $questions = new Question($questionObject->id, $questionObject->questionText, $questionObject->answers);
+foreach ($questionsJson as $questionObject) {
+    foreach($questionObject as $answers) {
+        foreach($answers as $answer) {
+            $answerArray[] = $answer;
+        }
+    }
+    $questions = new Question ($questionObject->id, $questionObject->questionText, $answerArray);
+    var_dump($questions);
+    $answerArray = array();
 }
