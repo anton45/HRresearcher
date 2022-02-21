ae505ba6216be606631e5d513b105e9ade6b03b6
Anton Goleusov <antongoleusov@gmail.com>
2022-02-21 21:17:29 +0300

    last

--- index.php
+++ index.php
@@ -16,32 +16,40 @@ require_once __DIR__ . "/vendor/autoload.php";
  * Сериализую
  * Записываю в Json
  */
-echo 'sdfsdf';
+$pdo = new PDO("pgsql:host=localhost;dbname = hrresearch","anton_galeusov", "1212");
+$stmt = $pdo->prepare("SELECT * FROM answer");
+$stmt->execute();
 
-$postData = file_get_contents('php://input');
-$jsonBody = json_decode($postData, true);
-var_dump($_SERVER["REQUEST_URI"]);
-var_dump($GLOBALS["jsonBody"]);
-var_dump($_SERVER["REQUEST_METHOD"]);
-if ($_SERVER["REQUEST_METHOD"] === "PUT") {
-    createAnswer($jsonBody["id"], $jsonBody["answerText"], $jsonBody["point"]);
-}
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    readAnswer($jsonBody["id"]);
-}
-if ($_SERVER["REQUEST_METHOD"] === "PATH") {
-    updateAnswer($jsonBody["id"], $jsonBody["answerText"], $jsonBody["point"]);
-}
-if ($_SERVER["REQUEST_METHOD"] === "DELETE") {
-    deleteAnswer($jsonBody["id"]);
-}
+var_dump($stmt->fetchAll());
+//$test = "INSERT INTO answer (id, answer_text, point) VALUES (10, 'sdfsfd', 34);";ext-pdo' is
+//$connection->exec($test);
+//echo 'sdfsdf';
+//print_r(PDO::getAvailableDrivers());
 
-
-function getJson(string $fileName) {
-    $jsonString = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $fileName);
-    $json = json_decode($jsonString);
-    return $json;
-}
+//$postData = file_get_contents('php://input');
+//$jsonBody = json_decode($postData, true);
+//var_dump($_SERVER["REQUEST_URI"]);
+//var_dump($GLOBALS["jsonBody"]);
+//var_dump($jsonBody["id"]);
+//if ($_SERVER["REQUEST_METHOD"] === "PUT") {
+//    createAnswer($jsonBody["id"], $jsonBody["answerText"], $jsonBody["point"]);
+//}
+//if ($_SERVER["REQUEST_METHOD"] === "GET") {
+//    readAnswer($jsonBody["id"]);
+//}
+//if ($_SERVER["REQUEST_METHOD"] === "PATCH") {
+//    return updateAnswer($jsonBody["id"], $jsonBody["answerText"], $jsonBody["point"]);
+//}
+//if ($_SERVER["REQUEST_METHOD"] === "DELETE") {
+//    deleteAnswer($jsonBody["id"]);
+//}
+//
+//
+//function getJson(string $fileName) {
+//    $jsonString = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $fileName);
+//    $json = json_decode($jsonString);
+//    return $json;
+//}
 
 // Test class Answer
 //function testclassAnswer()
@@ -134,69 +142,77 @@ function getJson(string $fileName) {
 //}
 //
 // Добавление нового ответа
-function createAnswer(int $id, string $answerText, int $point) {
-    $jsonAnswer = getJson('answers.json');
-    $createAnswer = new Answer($id, $answerText, $point);
-    $jsonAnswer[] = ["id" => $createAnswer->getId(), "answerText" => $createAnswer->getanswerText(), "point" => $createAnswer->getPoint()];
-    $newAnsweroffnull = array_filter($jsonAnswer);;
-        $newAnsweroffnulljson = json_encode($newAnsweroffnull);
-        $safeAnswer = file_put_contents('answers.json', $newAnsweroffnulljson);
-
-        return true;
-}
+//function createAnswer(int $id, string $answerText, int $point) {
+//    $jsonAnswer = getJson('answers.json');
+//    $createAnswer = new Answer($id, $answerText, $point);
+//    $jsonAnswer[] = ["id" => $createAnswer->getId(), "answerText" => $createAnswer->getanswerText(), "point" => $createAnswer->getPoint()];
+//    $newAnsweroffnull = array_filter($jsonAnswer);;
+//        $newAnsweroffnulljson = json_encode($newAnsweroffnull);
+//        $safeAnswer = file_put_contents('answers.json', $newAnsweroffnulljson);
+//
+//        return true;
+//}
 //createAnswer(77, 'test', 16);
 
 // Чтение ответов
-function readAnswer(array $arrayId) {
-    $jsonAnswer = getJson('answers.json');
-    foreach ($jsonAnswer as $answerObject) {
-        foreach ($arrayId as $id){
-            if ($answerObject->id === $id) {
-                $readAnswer[] = ["id" => $answerObject->id, "answerText"=> $answerObject->answerText, "point"=>$answerObject->point];
-            }
-        }
-    }
-    var_dump($readAnswer);
-    return $readAnswer;
-}
+//function readAnswer(array $arrayId) {
+//    $jsonAnswer = getJson('answers.json');
+//    foreach ($jsonAnswer as $answerObject) {
+//        foreach ($arrayId as $id){
+//            if ($answerObject->id === $id) {
+//                $readAnswer[] = ["id" => $answerObject->id, "answerText"=> $answerObject->answerText, "point"=>$answerObject->point];
+//            }
+//        }
+//    }
+//    var_dump($readAnswer);
+//    return $readAnswer;
+//}
 //readAnswer([1,3]);
 
 // Редактирование ответа
-function updateAnswer(int $id, string $answerText, int $point) {
-    $jsonAnswer = getJson('answers.json');
-    var_dump($jsonAnswer);
-    $updateAnswer = new Answer($id, $answerText, $point);
-    foreach ($jsonAnswer as $answerObject) {
-        if ($answerObject->id === $updateAnswer->getId()) {
-            $answerObject->answerText = $updateAnswer->getanswerText();
-            $answerObject->point = $updateAnswer->getPoint();
-        }
-    }
-    $readanswerJson = json_encode($jsonAnswer);
-    $safeAnswer = file_put_contents('answers.json', $readanswerJson);
-
-    return $safeAnswer;
-}
-//updateAnswer(10, 'DDDDDDDDDDDd', 34);
-
-function deleteAnswer(array $arrayId)
-{
-    $jsonAnswer = getJson('answers.json');
-    foreach ($jsonAnswer as $answerObject) {
-        if (!in_array($answerObject->id, $arrayId)) {
-            $validAnswer[] = ["id" => $answerObject->id, "answerText" => $answerObject->answerText, "point" => $answerObject->point];
-        }
-    }
-    $validanswerJson = json_encode($validAnswer);
-//    if ($jsonAnswer === $validAnswer) {
-//        print_r('Id not found');
+//function updateAnswer(int $id, string $answerText, int $point)
+//{
+//    if (strlen($answerText) <= 1) {
+//        print_r('Text answer not found');
 //        return false;
 //    }
-    $safeAnswer = file_put_contents('answers.json', $validanswerJson);
-    return true;
-}
+//    $jsonAnswer = getJson('answers.json');
+//    $updateAnswer = new Answer($id, $answerText, $point);
+//    foreach ($jsonAnswer as $answerObject) {
+//        if ($answerObject->id === $updateAnswer->getId()) {
+//            $answerObject->answerText = $updateAnswer->getanswerText();
+//            $answerObject->point = $updateAnswer->getPoint();
+//        }
+//    }
+//    $readanswerJson = json_encode($jsonAnswer);
+//    $safeAnswer = file_put_contents('answers.json', $readanswerJson);
+//
+//    return true;
+//}
 
-//deleteAnswer([8,5]);
+//updateAnswer(1, 'sdfsf', 54);
+
+//function deleteAnswer(array $arrayId)
+//{
+//    if (count($arrayId) < 1) {
+//        print_r('No elements found in the array');
+//        return false;
+//    }
+//    $jsonAnswer = getJson('answers.json');
+//    var_dump($jsonAnswer);
+//    foreach ($jsonAnswer as $answerObject) {
+//        if (!in_array($answerObject->id, $arrayId)) {
+//            $validAnswer[] = ["id" => $answerObject->id, "answerText" => $answerObject->answerText, "point" => $answerObject->point];
+//        }
+//    }
+//    $validanswerJson = json_encode($validAnswer);
+//    var_dump($validanswerJson);
+//    $safeAnswer = file_put_contents('answers.json', $validanswerJson);
+//    print_r('true');
+//    return true;
+//}
+
+//deleteAnswer([]);
 
 
 //$answer1 = new Answer(10, 'TestTest', 20);
